ac0da4cf5b55e76172416e6885f9a86b69753707
Pearse Graffin <2263964g@student.gla.ac.uk>
2018-02-05 14:27:37 +0000

    fifth commit

--- populate_rango.py
+++ populate_rango.py
@@ -10,23 +10,31 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url": "http://docs.python.org/2/tutorial/"},
+         "url": "http://docs.python.org/2/tutorial/",
+         "views": 333},
         {"title": "How to Think like a Computer Scientist",
-         "url": "http://greenteapress.com/thinkpython/"},
+         "url": "http://greenteapress.com/thinkpython/",
+         "views": 444},
         {"title": "Learn Python in 10 Minutes",
-         "url": "http://www.korokithakis.net/tutorials/python/"} ]
+         "url": "http://www.korokithakis.net/tutorials/python/",
+         "views": 555} ]
     django_pages = [
         {"title": "Official Django Tutorial",
-         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 123},
         {"title": "Django Rocks",
-         "url": "http://www.djangorocks.com/"},
+         "url": "http://www.djangorocks.com/",
+         "views": 5435},
         {"title": "How to Tango with Django",
-         "url": "http://www.tangowithdjango.com/"} ]
+         "url": "http://www.tangowithdjango.com/",
+         "views": 23423} ]
     other_pages = [
         {"title": "Bottle",
-         "url": "http://bottlepy.org/docs/dev/"},
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 6456},
         {"title": "Flask",
-         "url": "http://flash.pocoo.org"} ]
+         "url": "http://flash.pocoo.org",
+         "views": 5255} ]
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages, "views": 64, "likes": 32},
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
@@ -40,10 +48,9 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
+def add_page(cat, title, url, views):
+    p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
     p.url=url
-    p.views=views
     p.save()
     return p
 
